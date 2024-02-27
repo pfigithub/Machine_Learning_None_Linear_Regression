2c8e9271e2f719802d7e9f5b7405e5bb7719bc96
pfigithub <pampam7072@gmail.com>
2024-02-27 16:38:38 +0330

    plot

--- none-linear-regression.py
+++ none-linear-regression.py
@@ -6,3 +6,11 @@ import pandas as pd
 df = pd.read_csv("china_gdp.csv")
 df.head(10)
 
+# ploting data
+import matplotlib.pyplot as plt
+plt.figure(figsize=(8,5))
+x_data, y_data = (df["Year"].values, df["Value"].values)
+plt.plot(x_data, y_data, 'ro')
+plt.ylabel('GDP')
+plt.xlabel('Year')
+plt.show()
