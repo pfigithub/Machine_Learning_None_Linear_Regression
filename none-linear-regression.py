85a683f6d8e80d49038bf0c7242abd97211bd197
pfigithub <pampam7072@gmail.com>
2024-02-27 16:42:44 +0330

    finding the best model

--- none-linear-regression.py
+++ none-linear-regression.py
@@ -23,3 +23,29 @@ plt.plot(X,Y)
 plt.ylabel('Dependent Variable')
 plt.xlabel('Independent Variable')
 plt.show()
+
+# building the model
+def sigmoid(x, Beta_1, Beta_2):
+     y = 1 / (1 + np.exp(-Beta_1*(x-Beta_2)))
+     return y
+
+beta_1 = 0.10
+beta_2 = 1990.0
+
+# logistic function
+Y_pred = sigmoid(x_data, beta_1 , beta_2)
+
+
+# plot initial prediction against datapoints
+plt.plot(x_data, Y_pred*15000000000000.)
+plt.plot(x_data, y_data, 'ro')
+
+# Lets normalize our data
+xdata =x_data/max(x_data)
+ydata =y_data/max(y_data)
+
+
+from scipy.optimize import curve_fit
+popt, pcov = curve_fit(sigmoid, xdata, ydata)
+# print the final parameters
+print(" beta_1 = %f, beta_2 = %f" % (popt[0], popt[1]))
